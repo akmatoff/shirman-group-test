040b968c4848da08391ddebf393704cc1776b066
akmatoff <azim.akmatov1@gmail.com>
2025-04-15 18:02:05 +0600

    update header colors

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="w-full p-2 md:p-4 border-b">
       <div className="flex space-x-3 items-center">
         <ShoppingBasketIcon className="w-8 h-8 text-primary" />
-        <h1 className="text-xl font-bold text-primary">Shirman Group</h1>
+        <h1 className="text-xl font-bold">Shirman Group</h1>
       </div>
     </header>
   );
